f614342a78ac051a5fba70095f90b77318a31ba6
James King <j@lostwith.me>
2018-04-27 19:55:55 +0100

    progress

--- app/scripts/grid.js
+++ app/scripts/grid.js
@@ -1,105 +1,115 @@
 'use strict';
 
-var Grid = function(width, height) {
-  this.width = width || 10;
-  this.height = height || 10;
-
-  this.data = [];
-
-  this.build();
-  // this.print();
-};
-
-Grid.prototype.build = function() {
-  for (var y = 0; y < this.height; y++) {
-    this.data.push([]);
-
-    for (var x = 0; x < this.width; x++) {
-      this.data[y].push(' ');
-    }
-  }
-};
-
-Grid.prototype.print = function() {
-  var line,
-    output = '';
-
-  for (var y = 0; y < this.height; y++) {
-    line = '';
-
-    for (var x = 0; x < this.width; x++) {
-      line += '[' + this.data[y][x] + ']';
-    }
-
-    output += line + '\n';
+class Grid {
+  constructor(width, height) {
+    this.width = width || 30;
+    this.height = height || 30;
+    this.data = [];
+    this.build();
+    this.print();
   }
 
-  console.log(output);
-};
-
-Grid.prototype.query = function(pos) {
-  var options,
-    output = [];
-
-  if (this.empty(pos)) {
-    return true;
-  } else {
-    options = [
-      { x: pos.x, y: pos.y - 1, dir: 'north' },
-      { x: pos.x + 1, y: pos.y, dir: 'east' },
-      { x: pos.x, y: pos.y + 1, dir: 'south' },
-      { x: pos.x - 1, y: pos.y, dir: 'west' },
-    ];
-
-    for (var i = 0; i < options.length; i++) {
-      if (this.empty(options[i])) {
-        output.push(options[i]);
+  build() {
+    for (var y = 0; y < this.height; y++) {
+      this.data.push([]);
+      for (var x = 0; x < this.width; x++) {
+        this.data[y].push(' ');
       }
     }
   }
 
-  return output;
-};
+  // print() {
+  //   var line,
+  //     output = '';
+  //   for (var y = 0; y < this.height; y++) {
+  //     line = '';
+  //     for (var x = 0; x < this.width; x++) {
+  //       line += '[' + this.data[y][x] + ']';
+  //     }
+  //     output += line + '\n';
+  //   }
+  //   console.log(output);
+  // }
 
-Grid.prototype.empty = function(pos) {
-  if (
-    pos.x < this.width &&
-    pos.x >= 0 &&
-    pos.y < this.height &&
-    pos.y >= 0 &&
-    this.data[pos.y][pos.x] === ' '
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-};
+  print() {
+    const hole = d3.select('.hole');
 
-Grid.prototype.mark = function(pos) {
-  var mark;
+    const rows = hole
+      .selectAll('path')
+      .data(this.data)
+      .enter()
+      .append('g');
+    // .attr('d', d => d);
 
-  switch (pos.dir) {
-    case 'origin':
-      mark = 'x';
-      break;
-    case 'terminate':
-      mark = 'o';
-      break;
-    case 'north':
-      mark = '^';
-      break;
-    case 'south':
-      mark = 'v';
-      break;
-    case 'east':
-      mark = '>';
-      break;
-    case 'west':
-      mark = '<';
-      break;
-    default:
-      console.log('unknown direction');
+    const cells = rows
+      .selectAll('g')
+      .data(d => d)
+      .enter()
+      .append('rect')
+      .attr('width', d => 50)
+      .attr('height', d => 50);
+    // .data(gridData)
   }
 
-  this.data[pos.y][pos.x] = mark;
-};
+  query(pos) {
+    var options,
+      output = [];
+    if (this.empty(pos)) {
+      return true;
+    } else {
+      options = [
+        { x: pos.x, y: pos.y - 1, dir: 'north' },
+        { x: pos.x + 1, y: pos.y, dir: 'east' },
+        { x: pos.x, y: pos.y + 1, dir: 'south' },
+        { x: pos.x - 1, y: pos.y, dir: 'west' },
+      ];
+      for (var i = 0; i < options.length; i++) {
+        if (this.empty(options[i])) {
+          output.push(options[i]);
+        }
+      }
+    }
+    return output;
+  }
+
+  empty(pos) {
+    if (
+      pos.x < this.width &&
+      pos.x >= 0 &&
+      pos.y < this.height &&
+      pos.y >= 0 &&
+      this.data[pos.y][pos.x] === ' '
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  mark(pos) {
+    var mark;
+    switch (pos.dir) {
+      case 'origin':
+        mark = 'x';
+        break;
+      case 'terminate':
+        mark = 'o';
+        break;
+      case 'north':
+        mark = '^';
+        break;
+      case 'south':
+        mark = 'v';
+        break;
+      case 'east':
+        mark = '>';
+        break;
+      case 'west':
+        mark = '<';
+        break;
+      default:
+        console.log('unknown direction');
+    }
+    this.data[pos.y][pos.x] = mark;
+  }
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,145 +1,122 @@
 'use strict';
 /* globals Grid, Line, $, d3 */
 
-var Splash = function() {
-  this.height = 0;
-  this.width = 0;
+class Splash {
+  constructor() {
+    this.height = 0;
+    this.width = 0;
+    this.lineNum = 16;
 
-  this.lineNum = 16;
-
-  // this.init();
-};
-
-Splash.prototype.init = function() {
-  this.getDimensions();
-
-  var grid = new Grid(this.width, this.height);
-
-  this.lines = [];
-
-  var mid = Math.round(this.width / 2) - Math.floor(this.lineNum / 4);
-  var mid2 = Math.round(this.height / 2);
-
-  for (var i = 0; i < this.lineNum / 2; i++) {
-    this.lines.push(new Line(grid, { x: mid + i, y: mid2 }));
+    // this.init();
   }
 
-  for (i = 0; i < this.lineNum / 2; i++) {
-    this.lines.push(new Line(grid, { x: mid + i, y: mid2 - 1 }));
+  init() {
+    this.getDimensions();
+
+    console.log('this.width: ', this.width);
+    var grid = new Grid(this.width, this.height);
+
+    this.lines = [];
+
+    var mid = Math.round(this.width / 2) - Math.floor(this.lineNum / 4);
+    var mid2 = Math.round(this.height / 2);
+
+    for (var i = 0; i < this.lineNum / 2; i++) {
+      this.lines.push(new Line(grid, { x: mid + i, y: mid2 }));
+    }
+
+    for (i = 0; i < this.lineNum / 2; i++) {
+      this.lines.push(new Line(grid, { x: mid + i, y: mid2 - 1 }));
+    }
+
+    for (i = 0; i < 250; i++) {
+      this.tick();
+    }
+    // grid.print();
+    this.draw();
   }
 
-  for (i = 0; i < 250; i++) {
-    this.tick();
+  getDimensions() {
+    const getDiag = (a, b) => Math.round(Math.sqrt(a * a + b * b));
+
+    this.win = {
+      width: $(window).width(),
+      height: $(window).height(),
+    };
+
+    this.win.diag = getDiag(this.win.width, this.win.height);
+
+    // $('.hole').css({
+    // height: this.win.diag,
+    // width: this.win.diag,
+    // 'margin-left': -this.win.diag / 2,
+    // 'margin-top': -(this.win.diag - this.win.height) / 2,
+    // });
+
+    // this.height = this.win.diag / 30;
+    // this.width = this.win.diag / 30;
+
+    this.height = this.width = 10;
   }
 
-  // grid.print();
-  this.draw();
-};
-
-Splash.prototype.getDimensions = function() {
-  var getDiag = function(a, b) {
-    return Math.round(Math.sqrt(a * a + b * b));
-  };
-
-  this.win = {
-    width: $(window).width(),
-    height: $(window).height(),
-  };
-
-  this.win.diag = getDiag(this.win.width, this.win.height);
-
-  $('.hole').css({
-    height: this.win.diag,
-    width: this.win.diag,
-    'margin-left': -this.win.diag / 2,
-    'margin-top': -(this.win.diag - this.win.height) / 2,
-  });
-
-  this.height = this.win.diag / 30;
-  this.width = this.win.diag / 30;
-};
-
-Splash.prototype.tick = function() {
-  for (var i = 0; i < this.lines.length; i++) {
-    this.lines[i].move();
-  }
-};
-
-Splash.prototype.timer = function() {
-  window.setInterval(function() {}, 100);
-};
-
-Splash.prototype.draw = function() {
-  var hole = {
-    height: $('.hole').height(),
-    width: $('.hole').width(),
-    padding: 50,
-  };
-
-  var svg = d3
-    .select('.hole')
-    .append('svg')
-    .attr('height', hole.height)
-    .attr('width', hole.width)
-    .append('g')
-    .attr('transform', 'translate(50,50)');
-
-  var scale = {
-    x: d3.scale
-      .linear()
-      .domain([0, this.width - 1])
-      .rangeRound([0, hole.width - hole.padding * 2]),
-
-    y: d3.scale
-      .linear()
-      .domain([0, this.height - 1])
-      .rangeRound([0, hole.height - hole.padding * 2]),
-  };
-
-  var lineMath = d3.svg
-    .line()
-    .x(function(d) {
-      return scale.x(d.x);
-    })
-    .y(function(d) {
-      return scale.y(d.y);
-    });
-
-  var paths = svg
-    .selectAll('path')
-    .data(this.lines)
-    .enter()
-    .append('path')
-    .attr('stroke-linecap', 'square')
-    .attr('stroke-width', 12)
-    .attr('d', function(d) {
-      return lineMath(d.history);
-    });
-
-  for (var i = 0; i < paths[0].length; i++) {
-    this.lines[i].pathLength = paths[0][i].getTotalLength() + 10;
+  tick() {
+    // for (var i = 0; i < this.lines.length; i++) {
+    // this.lines[i].move();
+    // }
+    this.lines.forEach(line => line.move());
   }
 
-  paths
-    .attr('stroke-dasharray', function(d) {
-      return d.pathLength + ' ' + d.pathLength;
-    })
-    .attr('stroke-dashoffset', function(d) {
-      return d.pathLength;
-    });
+  draw() {
+    var hole = {
+      height: $('.hole').height(),
+      width: $('.hole').width(),
+      // padding: 50,
+    };
 
-  window.setTimeout(function() {
-    paths
-      .transition()
-      .duration(4000)
-      .delay(function(d, index) {
-        return index * 100;
-      })
-      .ease('linear')
-      .attr('stroke-dashoffset', 0);
-  }, 2000);
-};
+    const svg = d3.select('.hole');
 
-var splash = new Splash();
+    const scale = {
+      x: d3.scale
+        .linear()
+        .domain([0, this.width - 1])
+        .rangeRound([0, hole.width]),
+      y: d3.scale
+        .linear()
+        .domain([0, this.height - 1])
+        .rangeRound([0, hole.height]),
+    };
+
+    var lineMath = d3.svg
+      .line()
+      .x(d => scale.x(d.x))
+      .y(d => scale.y(d.y));
+
+    var paths = svg
+      .selectAll('path')
+      .data(this.lines)
+      .enter()
+      .append('path')
+      .attr('d', d => lineMath(d.history));
+
+    for (var i = 0; i < paths[0].length; i++) {
+      this.lines[i].pathLength = paths[0][i].getTotalLength();
+    }
+
+    // paths
+    //   .attr('stroke-dasharray', d => d.pathLength + ' ' + d.pathLength)
+    //   .attr('stroke-dashoffset', d => d.pathLength);
+
+    // window.setTimeout(() => {
+    // paths
+    // .transition()
+    // .duration(4000)
+    // .delay((d, index) => index * 100)
+    // .ease('linear')
+    // .attr('stroke-dashoffset', 0);
+    // }, 2000);
+  }
+}
+
+const splash = new Splash();
 
 splash.init();
